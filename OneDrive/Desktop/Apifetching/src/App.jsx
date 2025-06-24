20cac7e7398b5e615312ee2e0b6688f2b975c9c3
rinshad7518 <rinshadcontacts@gmail.com>
2025-06-24 15:48:30 +0530

    Clean initial commit

---
+++ OneDrive/Desktop/Apifetching/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Apifetch from "./components/ApiFetch"
+function App() {
+  
+  return (
+    <>
+  <Apifetch></Apifetch>
+    </>
+  )
+}
+
+export default App
